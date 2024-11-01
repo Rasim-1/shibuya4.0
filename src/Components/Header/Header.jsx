45de538f9bd4b8a3e502b92ecfa934bf51d70c81
Rasim-1 <husanbaevrasim315@gmail.com>
2024-11-01 19:00:45 +0500

    Add files via upload

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './Header.css'
+
+const Header = () => {
+
+const[active, setActive] = useState(false)
+
+const toggleBurger = () => {
+    setActive(!active)
+}
+
+ 
+
+  return (
+  <>
+  <header className="header">
+    <div className="container">
+        <div className="header__wrapp">
+            <img src="https://api.logobank.uz/media/logos_png/Uzum-01.png" alt="" className="logo" />
+            <div className={`header__menu ${active ? 'active' : ''}`}>
+             
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Contacts</a>
+            </div>
+            <div onClick={toggleBurger} className={`burger ${active ? 'active' : ''}`}>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+  </header>
+  
+  </>
+  )
+}
+
+export default Header
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+   <>
+   <section className='Hero'>
+    <div className="container">
+        <div className="hero__wrap">
+            <div className="hero__card card-1">
+                <div className="hero__text">
+                    <h3>Lorem, ipsum.</h3>
+                    <h3>OMAYEVA SHINDIYO </h3>
+                </div>
+                <h2>OMAYEVA SHINDIYO</h2>
+
+            </div>
+            <div className="hero__card card-2">
+                <div className="hero__text">
+                    <h3>Lorem, ipsum.</h3>
+                    <h3>OMAYEVA SHINDIYO </h3>
+                </div>
+                <h2>OMAYEVA SHINDIYO</h2>
+
+            </div>
+            <div className="hero__card card-3">
+                <div className="hero__text">
+                    <h3>Lorem, ipsum.</h3>
+                    <h3>OMAYEVA SHINDIYO </h3>
+                </div>
+                <h2>OMAYEVA SHINDIYO</h2>
+
+            </div>
+            <div className="hero__card card-4">
+                <div className="hero__text">
+                    <h3>Lorem, ipsum.</h3>
+                    <h3>OMAYEVA SHINDIYO </h3>
+                </div>
+                <h2>OMAYEVA SHINDIYO</h2>
+
+            </div>
+        </div>
+    </div>
+   </section>
+   
+   </>
+  )
+}
+
+export default Hero
